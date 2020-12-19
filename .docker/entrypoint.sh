07544c38822ca6fa6de827c20ac726705cc934ea
Francisco Wallison <franciscowallison@gmail.com>
2020-12-19 13:23:03 -0300

    iniciado o reset do projeto

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-# a imagem já está criada.
-echo "Instalando dependencias php..... DIR: $(pwd)"
+
 composer install
-echo "Instalando dependencias node....."
-npm install
-echo "Executando key:Generate....."
 php artisan key:generate
-echo "Executando migrate....."
 php artisan migrate
 php-fpm
